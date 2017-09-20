629005d6ef61a69425cf595db6db5130f645314e
anko94 <xudan94@mail.ru>
2017-09-20 16:24:54 +0300

    2 task

---
+++ lesson1/program2/__init__.py
@@ -0,0 +1,6 @@
+from lesson1.program2.point import Point
+from lesson1.program2.triangle import Triangle
+
+if __name__ == "__main__":
+    triangle1 = Triangle(Point(1,2,3), Point(4,5,6), Point(7,8,9))
+    triangle2 = Triangle(Point(21,22,23), Point(24,25,26), Point(27,28,29))
---
+++ lesson1/program2/triangle.py
@@ -0,0 +1,9 @@
+from lesson1.program2.point import Point
+
+
+class Triangle:
+
+    def __init__(self, point1: Point, point2: Point, point3: Point):
+        self.point1 = point1
+        self.point2 = point2
+        self.point3 = point3
