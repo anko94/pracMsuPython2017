8aa43287cbb6789c33e546e53ea5cc67d7a5ba03
anko0617 <anatolii.kolodiazhnyi@netcracker.com>
2017-09-26 17:09:03 +0300

    Do task 1 in lesson 2

--- lesson2/program1/__init__.py
+++ lesson2/program1/__init__.py
@@ -1,20 +1,19 @@
-list1 = [1,4,3,5,64,6,3,1,3,5,7,8,8]
-list2 = [1,4,3,9,2,3,5,4,6,4,5,6,3,1,3,5,7,7,8]
+def F(list1, list2):
+    if len(list1) == 0 or len(list2) == 0:
+        return 0
+    if list1[0] == list2[0]:
+        list1.remove(list1[0])
+        list2.remove(list2[0])
+        return F(list1, list2) + 1
+    else:
+        list3 = list1.copy()
+        list4 = list2.copy()
+        list3.remove(list3[0])
+        list4.remove(list4[0])
+        return max(F(list3, list2), F(list1, list4))
 
-n = len(list1)
-k = len(list2)
-F = []
-for i in range(n):
-    l = []
-    for j in range(k):
-        l.append(0)
-    F.append(l)
 
-for i in range(n):
-    for j in range(k):
-        if(list1[i] == list2[j]):
-            F[i][j] = F[i-1][j-1]+1
-        else:
-            F[i][j] = max(F[i-1][j], F[i][j-1])
+list1 = [1, 4, 3, 5, 64, 6, 3, 1, 3, 5, 7, 8, 8]
+list2 = [1, 4, 3, 9, 2, 3, 5, 4, 6, 4, 5, 6, 3, 1, 3, 5, 7, 7, 8]
 
-print(F[n-1][k-1])
+print(F(list1, list2))
