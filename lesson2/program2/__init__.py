cb9e566e7128ebefda56eeb9747c7292ad1bca26
anko94 <raion777>
2017-09-27 23:47:29 +0300

    Fixed bugs in task2

--- lesson2/program2/__init__.py
+++ lesson2/program2/__init__.py
@@ -1,19 +1,45 @@
 from lesson2.program2.notintexception import NotIntException
 
-def d(list, index, beforeIndex, k):
+def d(list, index, l):
     if index == len(list):
-        return k
-    if list[beforeIndex]<list[index]:
-        return d(list, index+1,index, k+1)
-    else:
-        return max(d(list, index+1, index, 1),d(list, index+1,beforeIndex, k))
+        return l
+    i = (len(l)/2).__int__()
+    up = len(l)
+    down = 0
+    a = list[index]
+    while i>=0 and not (l[i-1]<=a and l[i]>=a):
+        if a < l[i-1]:
+            up = i
+            i -= ((up-down)/2)
+        else :
+            down = i
+            i += ((up - down)/2)
+        i = i.__int__()
+    if i >= 0 and l[i-1]!=a:
+        l[i] = a
+    return d(list, index+1,l)
 
 
 if __name__ == "__main__":
 
-    list = [2,3,3,3,5,6,8,1,2,3,4]
+    list = [2,3,3,3,5,6,8,1,2,3,4,5,6]
 
     for i in range(len(list)):
         if not type(list[i]) is int:
-            raise NotIntException("Element of the list isn't int")
-    print(d(list,0,0,0))
+            raise NotIntException("Element of the list isn't integer")
+    l = []
+    r = 100000
+    for i in range(len(list)+1):
+        if i==0:
+            l.append(-r)
+        else:
+            l.append(r)
+    l = d(list,0,l)
+    i = 1
+    sum = 0
+    while(l[i]!=r):
+        sum+=1
+        i+=1
+    print(sum)
+
+
