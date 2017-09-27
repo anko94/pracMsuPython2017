04baa2a93e3d1cad423680ce3cc1a788bfceeaf6
masterctmm <raion7777>
2017-09-27 16:06:55 +0300

    Did task 2 in lesson 2.

--- lesson2/program2/__init__.py
+++ lesson2/program2/__init__.py
@@ -1,19 +1,19 @@
 from lesson2.program2.notintexception import NotIntException
 
-def d(list, index, beforeIndex):
+def d(list, index, beforeIndex, k):
     if index == len(list):
-        return 0
+        return k
     if list[beforeIndex]<list[index]:
-        return d(list, index+1,index) + 1
+        return d(list, index+1,index, k+1)
     else:
-        return max(d(list, index+1, index),d(list, index+1,beforeIndex))
+        return max(d(list, index+1, index, 1),d(list, index+1,beforeIndex, k))
 
 
 if __name__ == "__main__":
 
-    list = [2,3,3,3,5,6,1,2,3,4,5,6,7]
+    list = [2,3,3,3,5,6,8,1,2,3,4]
 
     for i in range(len(list)):
         if not type(list[i]) is int:
             raise NotIntException("Element of the list isn't int")
-    print(d(list,0,0))
+    print(d(list,0,0,0))
