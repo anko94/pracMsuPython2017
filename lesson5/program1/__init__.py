057d527368e9aaa155bd53caed0ecdaf2895ec1f
masterctmm <t>
2017-10-24 11:56:25 +0300

    Started doing tsks from lesson 5

--- lesson5/program1/__init__.py
+++ lesson5/program1/__init__.py
@@ -2,20 +2,43 @@ import threading
 
 
 class MyThread(threading.Thread):
-    def __init__(self, n, j, m, G, r, am):
+    def __init__(self, A, B, i, k, C):
         threading.Thread.__init__(self)
-        self.n = n
-        self.j = j
-        self.m = m
-        self.G = G
-        self.r = r
-        self.am = am
+        self.A = A
+        self.B = B
+        self.C = C
+        self.i = i
+        self.k = k
 
     def run(self):
         a = 0
-        for f in range(self.n):
-            if f != self.j:
-                a += self.m[f] * self.G * (self.r[f] - self.r[self.j]) / abs(self.r[f] - self.r[self.j]) ** 3
-        self.am.insert(self.j, a)
+        for j in range(len(self.A)):
+            a += self.A[j]*self.B[j]
+        self.C[self.i][self.k] = a
 
 if __name__ == "__main__":
+    A = [[1, 2, 3, 5], [3, 3, 4, 5], [1, 2 ,1, 5], [3, 4, 5, 3]]
+    B = [[1, 1], [3, 3], [4, 4], [3, 4]]
+    C = []
+    for i in range(len(A)):
+        E = [0] * len(B[0])
+        C.append(E)
+    if len(B) != len(A[0]):
+        print("wrong matrices")
+    else:
+        threads = []
+        k = 0
+        for i in range(len(B)):
+            A1 = A[i]
+            if k >= len(B[0]):
+                k = 0
+            B1 = []
+            for j in range(len(B)):
+                B1.append(B[j][k])
+            thread = MyThread(A1, B1, i, k, C)
+            threads.append(thread)
+            thread.start()
+            k += 1
+        for t in threads:
+            t.join()
+        print(C)
