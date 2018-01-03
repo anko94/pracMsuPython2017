715d93379043e0c49db4be5440e19eac4a193195
anatolii <xudan94@mail.ru>
2018-01-03 21:22:44 +0300

    Did fenics

--- fenicslesson/__init__.py
+++ fenicslesson/__init__.py
@@ -1,32 +1,35 @@
-from dolfin import *
-from dolfin.cpp.common import has_petsc
-from dolfin.cpp.io import File
-from dolfin.cpp.la import PETScOptions
+from fenics import *
 import numpy as np
 from matplotlib import tri, pyplot as plt
 
-
 if __name__ == "__main__":
-    mesh_size = 64
+    mesh_size = 40
     p1 = Point(0, 0)
-    p2 = Point(DOLFIN_PI, DOLFIN_PI)
+    p2 = Point(FENICS_PI, FENICS_PI)
     mesh = RectangleMesh(p1,p2,mesh_size,mesh_size)
-    V = FunctionSpace(mesh, "P", 1)
+    V = FunctionSpace(mesh, "P", 5)
+
+    # boundary_parts = FacetFunction('size_t', mesh)
+    # up = AutoSubDomain(lambda x: near(x[1], FENICS_PI, FENICS_EPS))
+    # bottom = AutoSubDomain(lambda x: near(x[1], 0, FENICS_EPS))
+    # up.mark(boundary_parts, 2)
+    # bottom.mark(boundary_parts, 2)
+    # dsn = Measure("ds", subdomain_id=2, subdomain_data=boundary_parts)
 
     # Define variational problem
     u = TrialFunction(V)
     v = TestFunction(V)
     f = Constant(0)
-    g = Expression("(pi-x[1])/pi*sin(4*x[0]) + x[1]/pi*sin(6*x[0])", degree=3)
+    g = Expression("(pi-x[1])/pi*sin(4*x[0]) - x[1]/pi*sin(6*x[0])", degree=5)
     a = dot(grad(u), grad(v)) * dx
     L = f * v * dx - g * v * ds
 
     # Define boundary condition values
-    u0 = Expression("x[0]/pi*cos(4*x[1])", degree=3)
+    u0 = Expression("x[0]/pi*cos(4*x[1])", degree=5)
 
     # Define boundary conditions
     def boundary1(x, on_boundary):
-        return on_boundary and (x[0] < DOLFIN_EPS or x[0] > DOLFIN_PI - DOLFIN_EPS)
+        return on_boundary and (near(x[0], 0, FENICS_EPS) or near(x[0], FENICS_PI, FENICS_EPS))
     bc0 = DirichletBC(V, u0, boundary1)
 
     # Set PETSc MUMPS paramter (this is required to prevent a memory error
@@ -40,8 +43,9 @@ if __name__ == "__main__":
 
     # Write solution to file
     File("u.pvd") << u
-
-    u_A = Expression("(1/4*sinh(4*x[1])-1/4*cosh(4*pi)/sinh(4*pi)*cosh(4*x[1]))*sin(4*x[0]) + 1/(6*sinh(6*pi))*cosh(6*x[1])*sin(6*x[0]) + sinh(4*x[0])/sinh(4*pi)*cos(4*x[1])", degree=3)
+    u_A = Expression(
+        "sinh(4*x[0])/sinh(4*pi)*cos(4*x[1])+(sinh(4*x[1])/4-cosh(4*pi)/(4*sinh(4*pi))*cosh(4*x[1]))*sin(4*x[0])+1/(6*sinh(6*pi))*cosh(6*x[1])*sin(6*x[0])",
+        degree=5)
     error_L2 = errornorm(u_A, u, 'L2')
     print(error_L2)
 
